76b80aaab4f4e8b87e7936cdbca278af9420ddba
OXplakhoa <OXplakhoa@gmail.com>
2025-01-02 11:51:13 +0700

    Add state between comps

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectSidebar";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    project: [],
+  });
+  const handleStartAddProj = () => {
+    setProjectState((prev) => {
+      return {
+        ...prev,
+        selectedProjectId: null,
+      };
+    });
+  };
   return (
     <>
       <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
       <main className="h-screen my-8 flex gap-8">
-        <ProjectSidebar/>
-        <NoProjectSelected/>
+        <ProjectSidebar onStartAppProj={handleStartAddProj} />
+        {projectState.selectedProjectId === null ? (
+          <NewProject />
+        ) : (
+          <NoProjectSelected onStartAppProj={handleStartAddProj} />
+        )}
       </main>
     </>
   );
--- src/components/ProjectSidebar.jsx
+++ src/components/ProjectSidebar.jsx
@@ -1,13 +1,13 @@
 import Button from "./Button";
 
-export default function ProjectSidebar() {
+export default function ProjectSidebar({ onStartAppProj }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-semibold md:text-2xl text-stone-300 uppercase">
         Your Project
       </h2>
       <div>
-        <Button>+ Add Project</Button>
+        <Button onClick={onStartAppProj}>+ Add Project</Button>
         <ul></ul>
       </div>
     </aside>
